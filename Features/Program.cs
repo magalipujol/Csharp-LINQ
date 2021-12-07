9c91683caef82ada93493917e630ab210cd9b504
Gali <Gali@DESKTOP-P4S8H6V>
2021-12-07 10:00:10 -0300

    Add project files.

---
+++ Features/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Gugu"},
+                new Employee { Id = 2, Name = "Guguis"}
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "gugipuki"}
+            };
+
+            
+        }
+    }
+}
