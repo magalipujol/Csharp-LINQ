9d6a5a95c13eec9832d7f634e68c6f0bed84dcaf
magali <magalipuuujol@gmail.com>
2021-12-07 11:13:15 -0300

    lambda practice

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using Features.Linq;
+
 
 namespace Features
 {
@@ -10,18 +10,51 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            //In Lambda expressions, the last parameter is the return type
+            //Func<int, int> square = x => x * x;
+            //Console.WriteLine(square(4));
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee { Id = 1, Name = "Gugu"},
-                new Employee { Id = 2, Name = "Guguis"}
+                new Employee { Id = 2, Name = "Guguis"},
+                new Employee { Id = 3, Name = "Agus"}
             };
 
-            List<Employee> sales = new List<Employee>()
+            //I could also use IEnumerable<Employee> instead of var
+            var sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "gugipuki"}
             };
 
-            
+            Console.WriteLine("Employees with 4 letters");
+            //query and query2 are equivalent
+            var query = developers.Where(e => e.Name.Length == 4)
+                                         .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 4
+                         orderby developer.Name
+                         select developer;
+
+            foreach (var employee in query2)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            //this is another way of doing the code above
+            //foreach (var employee in developers.Where(
+            //    e => e.Name.Length == 4)
+            //    .OrderBy(e => e.Name))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            //Console.WriteLine(sales.Count());
+            //IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
         }
+
     }
 }
