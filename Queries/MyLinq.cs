f083c1ed2d3ec03ff958d40308f878069f525bf8
magali <magalipuuujol@gmail.com>
2021-12-07 13:23:39 -0300

    queries practice

---
+++ Queries/MyLinq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    public static class MyLinq
+    {
+        //if I didn't have the this parameter, I would've to call the method
+        // like: Filter(source, predicate)
+        // now I can call it like: source.Filter(predicate)
+        // source is the enumerable, and predicate is the function
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source,
+                                               Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Queries/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Title = "Blancanieves y los 7 gu",
+                            Rating = 8.9f,
+                            Year = 2001},
+                new Movie { Title = "En busca de Gu",
+                            Rating = 10.2f,
+                            Year = 2013},
+                new Movie { Title = "GUardianes de la gualaxia",
+                            Rating = 2.7f,
+                            Year = 1999},
+                new Movie { Title = "Gu, mi villano favorito",
+                            Rating = 5.9f,
+                            Year = 2007},
+            };
+
+            var query = movies.Filter(m => m.Year > 2000);
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+    }
+}
