b8ba47bf894b8e08c40e0a3a4b3c4539378dc8b6
magali <magalipuuujol@gmail.com>
2021-12-13 09:48:58 -0300

    created a file processor

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cars
 {
@@ -6,7 +9,25 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcessFile("fuel.csv");
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        // this is a file processor
+        private static List<Car> ProcessFile(string path)
+        {
+            // to use the ReadAllLines method I have to use System.IO
+            // I give a path to a file and returns an array of string
+            // each string represents a line in the fuel.csv file
+            // the methods after (path) are linq mehtods, with use System.Linq
+            return File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .Select(Car.ParseFromCsv)
+                    .ToList();
         }
     }
 }
